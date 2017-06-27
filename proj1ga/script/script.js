c6809224fda3b2c9a6ffb57037963f56d3848f6c
Adam Roberts <aroberts85@yahoo.com>
2017-06-27 14:51:41 -0400

    start fresh whackashoebill

--- proj1ga/script/script.js
+++ proj1ga/script/script.js
@@ -1,71 +1,28 @@
 jQuery(function() {
 console.log("script loaded");
+let gameOver = true;
 
-// const gameBoard = document.createElement("div");
-//   gameBoard.setAttribute("id","board");
-//creates game board divs with id of board
-//use a for loop to create array of divs and hole adding each hole
 
-let play = false;
-let len = 24;
-let score = 0;
-let lilyDiv = new Array();
-  for(let i = 0; i < len; i++){//create lilyarray of divs
-    lilyDiv[i] = document.createElement("div");
-    lilyDiv[i].className = "lily";
-    document.body.appendChild(lilyDiv[i]);
+// start game
+$('.startButton').click(function()
+  {
+	   startGame();
+   });
+
+// timer when start button clicked
+function startGame(){
+  if(gameOver){
+    shoebillMove();
+    gameOver = false;
+    timer = setInterval(decrementTime, 1000);
   };
-
-
-$(".lily").wrapAll ("<div id = 'gameBoard'></div>");//wrap gameboard around .lily
-$("<img src= img/shobilltoon.png></img>" ).appendTo(".lily"); //append shoebilldiv to lilydiv
-$("img").addClass("shoebillToon");
-$("#gameBoard").wrapAll("<div id = 'scoreBoard'></div>")//create score display in new div
-$("<button> start</button>").appendTo("h1")
-let shoebillToon = document.querySelectorAll(".shoebillToon");
-let scoreBoard = document.getElementById("scoreBoard")
-function showScore(){
-  scoreBoard.innerHTML = "Score:" + score;
-
-}//function that holds scoreboard
-//make random function
-
-let random = function(){
-  return Math.floor(Math.random() * 10);
 }
-
-//game length function
-let gameLength = function(){
-  let lilyPad = random();
-  let lastScore = score;
-$(".shoebillToon")
-  setTimeout(function(){
-  if(score === lastScore){
-    showScore();
+// get random shoebill to pop up
+  function getNewLilly() {
+    let lilly = $(.lilly); //gets all lillys
+    let randomNumber = Math.floor(math.random()* 9);
+    let shoebillToAnimate = Lilly[randomNumber];
+    let id = shoebillToAnimate.getAttribute('id');
+      animateShoebill(id);
+      console.log(id);
   }
-}, 20000);
-}
-
-
-$("button").on("click",gameLength());//click start button to start game timer
-var images = $('img');
-var randomHide = function(image){
-  let alreadyClicked= $("img").click( function(){
-    $(this).hide();//hide shoebill images click
-     //when all shoebills are clicked endgame
-  });//if it has "already clicked class, then keep hidden"
-  let ranShow = Math.floor(Math.random() * 10);
-  if (ranShow === 1 ){
-    $(image).hide()
-    } else {
-      $(image).show()
-    }
-}
-
-
-$.each(images, function(){
-  console.log($(this));
-  setInterval(randomHide($(this)), 400);
-})
-
-})
